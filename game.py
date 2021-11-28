803ee38b901c33bfe7b5ddef9ac326cc9f8609a6
bferenczy <balint.ferenczy1@gmail.com>
2021-11-28 22:45:14 +0100

    Result screen added

--- game.py
+++ game.py
@@ -1,11 +1,10 @@
-from numpy import tan
 import pygame 
 from main_menu import MainMenu
+from result import Result
 from scores import Scores
 from new_game_menu import NewGameMenu
 from ui_elements import *
 from constants import Scenes
-import sys
 
 
 from controller.game_controller import GameController
@@ -34,8 +33,10 @@ def main():
         if scene is Scenes.GAME:
             controller = GameController(screen, newMenu.state)
             scene, result = controller.run()
-            from pprint import pprint
-            pprint(result)
+        if scene is Scenes.RESULT:
+            result = Result(screen, fps, result)
+            scene = result.run()
+
 
 
 if __name__ == "__main__":
